551285752d658a4b3bef09e5e5a3218b8ead1e71
wchamberlain8 <william.chamberlain8@gmail.com>
2024-11-12 14:13:07 +0000

    Made client code more elegant and updated AuctionServer to handle the new interface

--- AuctionServer.java
+++ AuctionServer.java
@@ -12,7 +12,6 @@ import java.security.PublicKey;
 import java.util.HashMap;
 import java.util.Random;
 
-
 public class AuctionServer implements Auction {
 
     private HashMap<Integer, AuctionItem> itemsMap = new HashMap<>();
@@ -21,160 +20,186 @@ public class AuctionServer implements Auction {
     private HashMap<Integer, Integer> highestBidderMap = new HashMap<>();
     Random random = new Random();
 
-    public AuctionServer() throws RemoteException{
+    public AuctionServer() throws RemoteException {
         super();
-    
+
     }
 
-    //Function to generate a KeyPair for cryptographic authentication later
-    private static void generateKeyPair() throws Exception{
-            try {
-                KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");  //set up the key generator
-                keyGen.initialize(2048);
-                KeyPair keyPair = keyGen.generateKeyPair();
-                PrivateKey serverPrivateKey = keyPair.getPrivate();     //generate and seperate the two keys
-                PublicKey serverPublicKey = keyPair.getPublic();
-    
-                FileOutputStream fileOut = new FileOutputStream("keys/server_public.key");  //save the public key (in bytes) to a file
-                fileOut.write(serverPublicKey.getEncoded());
-                fileOut.close();
-                
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+    // Function to generate a KeyPair for cryptographic authentication later
+    private static void generateKeyPair() throws Exception {
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA"); // set up the key generator
+            keyGen.initialize(2048);
+            KeyPair keyPair = keyGen.generateKeyPair();
+            PrivateKey serverPrivateKey = keyPair.getPrivate(); // generate and seperate the two keys
+            PublicKey serverPublicKey = keyPair.getPublic();
+
+            FileOutputStream fileOut = new FileOutputStream("keys/server_public.key"); // save the public key (in bytes)
+                                                                                       // to a file
+            fileOut.write(serverPublicKey.getEncoded());
+            fileOut.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    
-        //Helper function to create a new AuctionItem since constructor not allowed
-        public AuctionItem createItem(int itemID, String name, String description, int highestBid){
-            AuctionItem item = new AuctionItem();
-            item.itemID = itemID;
-            item.name = name;
-            item.description = description;
-            item.highestBid = highestBid;
-    
-            return item;
+    }
+
+    @Override
+    public ChallengeInfo challenge(int userID, String clientChallenge) throws RemoteException {
+        return null;
+    }
+
+
+    @Override
+    public TokenInfo authenticate(int userID, byte[] signature) throws RemoteException {
+        return null;
+    }
+
+    // Helper function to create a new AuctionItem since constructor not allowed
+    public AuctionItem createItem(int itemID, String name, String description, int highestBid) {
+        AuctionItem item = new AuctionItem();
+        item.itemID = itemID;
+        item.name = name;
+        item.description = description;
+        item.highestBid = highestBid;
+
+        return item;
+    }
+
+    @Override
+    public AuctionItem getSpec(int userID, int itemID, String token ) throws RemoteException {
+
+        //TODO: CHECK TOKEN IS VALID HERE
+
+
+        if (itemsMap.isEmpty()) { // return null if the hashmap is empty
+            return null;
         }
-    
-        @Override
-        public AuctionItem getSpec(int itemID) throws RemoteException {
-    
-            if(itemsMap.isEmpty()){    //return null if the hashmap is empty
-                return null;
-            }
-    
-            AuctionItem item = itemsMap.get(itemID); //Get the AuctionItem from the hashmap with the relative itemID
-            return item;
-           
-        }
-    
-        @Override
-        public int register(String email) throws RemoteException{
-            int userID = random.nextInt(100);   //generate random userID
-            usersMap.put(userID, email);    //place into hashmap, userID being the key for each email, incase we need a user's email
-            return userID;
-        }
-    
-        @Override
-        public int newAuction(int userID, AuctionSaleItem item) throws RemoteException{
-            
-            int itemID = random.nextInt(1000);   //generate random itemID
-            userAuctionsMap.put(userID, item);   //place into hashmap, userID being the key for each AuctionSaleItem, to know who created the auction
-            
-            //create new AuctionItem from AuctionSaleItem info
-    
-            AuctionItem newItem = createItem(itemID, item.name, item.description, 0);
-            itemsMap.put(itemID, newItem); //place into hashmap, the itemID being the key for the AuctionItem, so we have all items saved in one place 
-    
-            return itemID;
-    
-        }
-    
-        @Override
-        public AuctionItem[] listItems() throws RemoteException{
-    
-            AuctionItem[] array = itemsMap.values().toArray(new AuctionItem[0]); //turn the item hashmap into an array 
-            return array;
-    
-        }
-    
-        @Override
-        public AuctionResult closeAuction(int userID, int itemID){
-    
-            AuctionItem item = itemsMap.get(itemID);    //Check if the item exists 
-            if(item == null){
-                System.out.println("AuctionItem of that itemID does not exist");
-                return null;
-            }
-            
-            if (userAuctionsMap.containsKey(userID) && item.itemID == itemID) { //Check the user closing the auction is the one who created it
-    
-                AuctionSaleItem saleItem = userAuctionsMap.get(userID);
-    
-                if(item.highestBid >= saleItem.reservePrice){   //Check if the reserve price has been met
-    
-                    AuctionResult result = new AuctionResult(); //Create a new instance of an AuctionResult using details from various hashmaps
-                    int winningUserID = highestBidderMap.get(itemID);
-                    result.winningEmail = usersMap.get(winningUserID);
-                    result.winningPrice = item.highestBid;
-    
-                    itemsMap.remove(itemID);    //Remove the item from auction
-    
-                    return result;
-                }
-                else{
-                    System.out.println("Reserve price not met");
-                    return null;
-                }
-    
-            } 
-            else{
-                System.out.println("UserID does not match the given AuctionItem's itemID");
-                return null;
-            }
-    
-        }
-    
-        @Override
-        public boolean bid(int userID, int itemID, int price) throws RemoteException{
-                
-                AuctionItem item = itemsMap.get(itemID);    //Check that the item exists
-                if(item == null){
-                    System.out.println("AuctionItem of that itemID does not exist");
-                    return false;
-                }
+
+        AuctionItem item = itemsMap.get(itemID); // Get the AuctionItem from the hashmap with the relative itemID
+        return item;
+
+    }
+
+    @Override
+    public int register(String email, PublicKey pkey) throws RemoteException {
+
+        //TODO: ADD USERS PUBLIC KEY TO AN AUTHENTICATED USER HASHMAP HERE
+
+        int userID = random.nextInt(100); // generate random userID
+        usersMap.put(userID, email); // place into hashmap, userID being the key for each email, incase we need a
+                                     // user's email
+        return userID;
+    }
+
+    @Override
+    public int newAuction(int userID, AuctionSaleItem item, String token) throws RemoteException {
+
+        //TODO: CHECK TOKEN IS VALID HERE
+
+        int itemID = random.nextInt(1000); // generate random itemID
+        userAuctionsMap.put(userID, item); // place into hashmap, userID being the key for each AuctionSaleItem, to know
+                                           // who created the auction
+
+        // create new AuctionItem from AuctionSaleItem info
+
+        AuctionItem newItem = createItem(itemID, item.name, item.description, 0);
+        itemsMap.put(itemID, newItem); // place into hashmap, the itemID being the key for the AuctionItem, so we have
+                                       // all items saved in one place
+
+        return itemID;
+
+    }
+
+    @Override
+    public AuctionItem[] listItems(int userID, String token) throws RemoteException {
         
-                if(price > item.highestBid){    //Check that the amount offered is higher than the current bid
-                    item.highestBid = price;    //Update highest bid
-                    highestBidderMap.put(item.itemID, userID);   //place in hashmap, showing who the highest bidder (userID) is for each given itemID, this overwrites the previous highest bidder
-                    return true;
-                }
-                else{
-                    System.out.println("Bid is not higher than the current highest bid");
-                    return false;
-                }
+        //TODO: CHECK TOKEN IS VALID HERE
+
+        AuctionItem[] array = itemsMap.values().toArray(new AuctionItem[0]); // turn the item hashmap into an array
+        return array;
+
+    }
+
+    @Override
+    public AuctionResult closeAuction(int userID, int itemID, String token) {
+
+        //TODO: CHECK TOKEN IS VALID HERE
+
+        AuctionItem item = itemsMap.get(itemID); // Check if the item exists
+        if (item == null) {
+            System.out.println("AuctionItem of that itemID does not exist");
+            return null;
         }
-    
-    
-    
-    
-            
-        public static void main(String[] args) {
-            try {
-                AuctionServer a1 = new AuctionServer(); //creating a server with the name 'Auction'
-                String name = "Auction";
-    
-                Auction stub = (Auction) UnicastRemoteObject.exportObject(a1, 0);
-    
-                Registry registry = LocateRegistry.getRegistry();
-                registry.rebind(name, stub);
-    
-                generateKeyPair();  //generate a new keyPair
+
+        if (userAuctionsMap.containsKey(userID) && item.itemID == itemID) { // Check the user closing the auction is the
+                                                                            // one who created it
+
+            AuctionSaleItem saleItem = userAuctionsMap.get(userID);
+
+            if (item.highestBid >= saleItem.reservePrice) { // Check if the reserve price has been met
+
+                AuctionResult result = new AuctionResult(); // Create a new instance of an AuctionResult using details
+                                                            // from various hashmaps
+                int winningUserID = highestBidderMap.get(itemID);
+                result.winningEmail = usersMap.get(winningUserID);
+                result.winningPrice = item.highestBid;
+
+                itemsMap.remove(itemID); // Remove the item from auction
+
+                return result;
+            } else {
+                System.out.println("Reserve price not met");
+                return null;
+            }
+
+        } else {
+            System.out.println("UserID does not match the given AuctionItem's itemID");
+            return null;
+        }
+
+    }
+
+    @Override
+    public boolean bid(int userID, int itemID, int price, String token) throws RemoteException {
+
+        //TODO: CHECK TOKEN IS VALID HERE
+
+        AuctionItem item = itemsMap.get(itemID); // Check that the item exists
+        if (item == null) {
+            System.out.println("AuctionItem of that itemID does not exist");
+            return false;
+        }
+
+        if (price > item.highestBid) { // Check that the amount offered is higher than the current bid
+            item.highestBid = price; // Update highest bid
+            highestBidderMap.put(item.itemID, userID); // place in hashmap, showing who the highest bidder (userID) is
+                                                       // for each given itemID, this overwrites the previous highest
+                                                       // bidder
+            return true;
+        } else {
+            System.out.println("Bid is not higher than the current highest bid");
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            AuctionServer a1 = new AuctionServer(); // creating a server with the name 'Auction'
+            String name = "Auction";
+
+            Auction stub = (Auction) UnicastRemoteObject.exportObject(a1, 0);
+
+            Registry registry = LocateRegistry.getRegistry();
+            registry.rebind(name, stub);
+
+            generateKeyPair(); // generate a new keyPair
+
             System.out.println("Server ready");
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.err.println("Exception:");
             e.printStackTrace();
-  }
-}
-    
+        }
+    }
+
 }
--- Client.java
+++ Client.java
@@ -18,82 +18,113 @@ public class Client{
   
       return newItem;
     }
-  
-    public static void main(String[] args) {
 
-      if(args.length <1){
-        System.out.println("Usage: java Client <command> <args>");
-        System.out.println("Available commands: register, new, list, bid, close, getSpec");
-        System.out.println("\nExample usage:");
-        System.out.println("register: java Client register <email>");
-        System.out.println("new: java Client new <userID> <name> \"<description>\" <reservePrice>");
-        System.out.println("list: java Client list");
-        System.out.println("bid: java Client bid <userID> <itemID> <bid>");
-        System.out.println("close: java Client close <userID> <itemID>");
-        System.out.println("getSpec: java Client getSpec <itemID>");
-      }
-
-      try{
-        String name = "Auction";
-        Registry registry = LocateRegistry.getRegistry("localhost");
-        Auction server = (Auction) registry.lookup(name);   //find the server and connect
-  
-        switch (args[0]) {
-          case "register":
-            int userID = server.register(args[1]);
+    public static void register(Auction server, String[] args){
+          int userID = server.register(args[1]);
             System.out.println("Successfully registered. Your userID is: " + userID + " you will need this to use system functions.");
-            break;
+        }
 
-          case "new":
-            AuctionSaleItem item = createSaleItem(args[2], args[3], Integer.parseInt(args[4]));
-            int itemID = server.newAuction(Integer.parseInt(args[1]), item);
-            System.out.println("Successfully created new auction. Your itemID is: " + itemID + " you will need this to use system functions.");
-            break;
+    public static void newAuction(Auction server, String[] args){
+          AuctionSaleItem item = createSaleItem(args[2], args[3], Integer.parseInt(args[4]));
+          int itemID = server.newAuction(Integer.parseInt(args[1]), item);
+          System.out.println("Successfully created new auction. Your itemID is: " + itemID + " you will need this to use system functions.");
+        }
 
-          case "list":
-            AuctionItem[] auctionArray = server.listItems();
-            for (AuctionItem auctionItem : auctionArray) {
-              System.out.println("ItemID: " + auctionItem.itemID);
-              System.out.println("Name: " + auctionItem.name);
-              System.out.println("Description: " + auctionItem.description);
-              System.out.println("Highest Bid: " + auctionItem.highestBid);
-            }
-            break;
+    public static void list(Auction server){
+          AuctionItem[] auctionArray = server.listItems();
+          for (AuctionItem auctionItem : auctionArray) {
+            System.out.println("ItemID: " + auctionItem.itemID);
+            System.out.println("Name: " + auctionItem.name);
+            System.out.println("Description: " + auctionItem.description);
+            System.out.println("Highest Bid: " + auctionItem.highestBid);
+          }
+        }
+      
+    public static void bid(Auction server, String[] args){
+          boolean check = server.bid(Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]));
+          if(check){
+            System.out.println("Successfully placed bid.");
+          }
+          else{
+            System.out.println("Failed to place bid.");
+          }
+        }
 
-          case "bid":
-            boolean check = server.bid(Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]));
+    public static void close(Auction server, String[] args){
+          AuctionResult result = server.closeAuction(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
 
-            if(check){
-              System.out.println("Successfully placed bid.");
-            }
-            else{
-              System.out.println("Failed to place bid.");
-            }
-            break;
+          if(result == null){
+            System.out.println("There was a problem, either the reserve price has not yet been met, that item does not exist or the given userID did not create the auction");
+          }
+          else{
+            System.out.println("The auction has been closed");
+            System.out.println("Winning email: " + result.winningEmail);
+            System.out.println("Winning bid: " + result.winningPrice);
+          }
+        }
 
-          case "close":
-            AuctionResult result = server.closeAuction(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
-            
-            if(result == null){
-              System.out.println("There was a problem, either the reserve price has not yet been met, that item does not exist or the given userID did not create the auction");
-            }
-            else{
-              System.out.println("The auction has been closed");
-              System.out.println("Winning email: " + result.winningEmail);
-              System.out.println("Winning bid: " + result.winningPrice);
-            }
+    public static void getSpec(Auction server, String[] args){
+          AuctionItem retrievedItem = server.getSpec(Integer.parseInt(args[1]));
+          System.out.println("Name: " + retrievedItem.name);
+          System.out.println("Description: " + retrievedItem.description);
+          System.out.println("Highest Bid: " + retrievedItem.highestBid);
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+      //Main method to connect to the server and call functions via command line input
+        public static void main(String[] args) {
+    
+          if(args.length <1){
+            System.out.println("Usage: java Client <command> <args>");
+            System.out.println("Available commands: register, new, list, bid, close, getSpec");
+            System.out.println("\nExample usage:");
+            System.out.println("register: java Client register <email>");
+            System.out.println("new: java Client new <userID> <name> \"<description>\" <reservePrice>");
+            System.out.println("list: java Client list");
+            System.out.println("bid: java Client bid <userID> <itemID> <bid>");
+            System.out.println("close: java Client close <userID> <itemID>");
+            System.out.println("getSpec: java Client getSpec <itemID>");
+          }
+    
+          try{
+            String name = "Auction";
+            Registry registry = LocateRegistry.getRegistry("localhost");
+            Auction server = (Auction) registry.lookup(name);   //find the server and connect
+      
+            switch (args[0]) {
+              case "register":
+                register(server, args);
+              break;
 
-            break;
+              case "new":
+                newAuction(server, args);
+              break;
 
-          case "getSpec":
-            AuctionItem retrievedItem = server.getSpec(Integer.parseInt(args[1]));
-            System.out.println("Name: " + retrievedItem.name);
-            System.out.println("Description: " + retrievedItem.description);
-            System.out.println("Highest Bid: " + retrievedItem.highestBid);
-            break;
-            
-          default:
-            break;
+              case "list":
+                list(server);
+              break;
+
+              case "bid":
+                bid(server, args);
+              break;
+
+              case "close":
+                close(server, args);
+              break;
+
+              case "getSpec":
+                getSpec(server, args);
+              break;
+            default:
+              break;
         }
     }
     
