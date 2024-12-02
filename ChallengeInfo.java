03bbd863233a9e84619f9573e688c6c06e35ffbc
chambe28 <w.chamberlain1@lancaster.ac.uk>
2024-12-02 15:31:22 +0000

    Removed unneccesary files - coursework complete

--- ChallengeInfo.java
+++
@@ -1,4 +0,0 @@
-public class ChallengeInfo implements java.io.Serializable {
-    byte response[]; // server’s response (signature) to client’s challenge
-    String serverChallenge; // server’s challenge to the client
-    }
--- server.sh
+++ server.sh
@@ -1,6 +1,6 @@
 javac *.java &
 rmiregistry & 
-sleep 1
+sleep 1 &
 java Replica 1 & 
 java Replica 2 &
 java Replica 3 &
